16943dc4e6cc1e0659f7b92b3e619b585828cc4a
NirmalKSingh <nirmalksingh@gmail.com>
2015-11-17 21:07:23 -0500

    just a comment, minor change can be ignored - no need to accept

--- www/app/app.js
+++ www/app/app.js
@@ -3,7 +3,7 @@ angular.module('mobilehope', ['ionic', 'starter.controllers', 'starter.services'
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+      // for form inputs) //comment
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
